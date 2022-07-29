9c766f1be2368e46b144750cebe2299af08df9a6
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-07-30 00:39:32 +0300

    a commit on the navigation media query

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -38,31 +38,7 @@ function BookDetails() {
     },[id])
     
   return (
-    <div>
-           <div className='flex'>
-              <div className='left'>
-                <img style={{width:300, height:500}} src={image} alt=""/>
-              </div>
-                <div className='right'>
-                   <br/>
-                   <div className='negative title'><i style={{color:"black"}}>TITLE:</i> {title}</div>
-                   <br/>
-                   <br/>
-                   <div className='negative author'><i style={{color:"black"}}>AUTHOR:</i> {author}</div>
-                   <br/>
-                   <br/>
-                   <div className='negative price'><i style={{color:"black"}}>PRICE:</i> {price}</div> 
-                   <br/>
-                   <br/>
-                   <div className='review'><i style={{color:"black"}}>REVIEW:</i> {review}</div>
-                   <br/>
-                   <br/>
-                   <br/>
-                   <h3 style={{color:"gray"}}><i>SYNOPSIS</i></h3>
-                   <p className='negative description' >{description}</p>
-                </div>
-           </div>
-    </div>
+    {}
   )
 }
 
