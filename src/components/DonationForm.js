7cabe3d0b1526e561f62a31bb3c550bc2bcf61f3
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-07-30 00:47:27 +0300

    a commit on the comment post form

--- src/components/DonationForm.js
+++ src/components/DonationForm.js
@@ -33,7 +33,29 @@ function DonationForm({onAddComment}) {
 
   return (
     <div className='cmleft'>
-   
+    <form className="comment-form" onSubmit={handleSubmit} >
+       <input className='commentInput'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        name="title" type="text" 
+        required placeholder="title" />
+       <input className='commentInput'
+        value={review}
+        onChange={(e) => setReview(e.target.value)}
+        name="review" type="text" 
+        required  placeholder="rate out of 5" />
+       <input className='commentInput'  
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        name="name" type="text" 
+        required placeholder="name" />
+       <textarea className='commentInput'  
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        name="content" type="text" 
+        required  placeholder="Write your comment here..." rows={10} />
+       <input className='commentInput-btn' type="submit"  value="Share your masterpiece" />
+    </form>
 </div>
 )
 
